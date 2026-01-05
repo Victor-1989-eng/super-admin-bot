372b5801602147b9788ab0f2120d87332a0f6548
Victor-1989-eng <ghnfhh061@gmail.com>
2026-01-05 22:15:42 +0200

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,2 +1,2 @@
 import os
-BOT_TOKEN = os.getenv("7988115767:AAFhpUf-DZDRpmI6ixFbw_-OB9AsPXdpOoQ") or "ВСТАВЬ_ТОКЕН_СЮДА"
+BOT_TOKEN = os.getenv("7988115767:AAFhpUf-DZDRpmI6ixFbw_-OB9AsPXdpOoQ") or "7988115767:AAFhpUf-DZDRpmI6ixFbw_-OB9AsPXdpOoQ"
