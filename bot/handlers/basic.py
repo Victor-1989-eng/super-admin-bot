20c39e679940b34ec1d5c00578a99e62c2b2e109
Victor-1989-eng <ghnfhh061@gmail.com>
2026-01-14 19:12:39 +0200

    Update basic.py

--- bot/handlers/basic.py
+++ bot/handlers/basic.py
@@ -1,6 +1,11 @@
+from aiogram import Router
+from aiogram.types import Message
 from aiogram.enums import ChatType
+
+router = Router()  # 🔥 ВОТ ЭТОГО НЕ ХВАТАЛО
 
 @router.message(lambda m: m.chat.type == ChatType.PRIVATE)
 async def ignore_private(message: Message):
     await message.answer("🤖 Бот работает только в группах")
+
 
